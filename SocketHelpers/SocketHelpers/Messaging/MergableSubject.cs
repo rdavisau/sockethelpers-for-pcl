83f955c577f5c6f8a453cf1c119d212a990de5a0
rdavisau <ryandavis.au@gmail.com>
2015-04-19 11:21:48 +1000

    add 'Unmerge' method to MergeableSubject

--- SocketHelpers/SocketHelpers/Messaging/MergableSubject.cs
+++ SocketHelpers/SocketHelpers/Messaging/MergableSubject.cs
@@ -10,17 +10,26 @@ namespace SocketHelpers.Messaging
         readonly Subject<T> _backingSub = new Subject<T>();
         public IObservable<T> SubscriptionLine { get { return _backingSub.AsObservable(); } }
 
-        readonly List<IDisposable> _subscriptions = new List<IDisposable>();
+        readonly Dictionary<object, IDisposable> _subscriptions = new Dictionary<object, IDisposable>();
 
-        public void Merge(IObservable<T> source)
+        public void Merge(IObservable<T> source, object key = null)
         {
             var sub = source.Subscribe(_backingSub.OnNext, _backingSub.OnError);
-            _subscriptions.Add(sub);
+            _subscriptions.Add(key ?? source, sub);
         }
 
+        public void Unmerge(object key)
+        {
+            IDisposable sub;
+            if (!_subscriptions.TryGetValue(key, out sub)) return;
+            
+            _subscriptions.Remove(key);
+            sub.Dispose();
+        }
+        
         public void Dispose()
         {
-            foreach (var sub in _subscriptions)
+            foreach (var sub in _subscriptions.Values)
                 sub.Dispose();
         }
     }
